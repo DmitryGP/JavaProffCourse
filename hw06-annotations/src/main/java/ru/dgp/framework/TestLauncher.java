e1de0e46580585e6f1b754c9ccb687764000770c
DmitryGP <dimgeo@yandex.ru>
2023-11-24 17:57:54 +0300

    Д.з. для лекции "Annotations".

---
+++ hw06-annotations/src/main/java/ru/dgp/framework/TestLauncher.java
@@ -0,0 +1,93 @@
+package ru.dgp.framework;
+
+import ru.dgp.annotations.After;
+import ru.dgp.annotations.Before;
+import ru.dgp.annotations.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class TestLauncher {
+
+    private TestLauncher(){}
+    public static void launch(Class<?> testClass){
+
+        Method[] beforeMethods = AnnotationHelper.getAnnotatedMethods(testClass, Before.class);
+        Method[] afterMethods = AnnotationHelper.getAnnotatedMethods(testClass, After.class);
+        Method[] tests = AnnotationHelper.getAnnotatedMethods(testClass, Test.class);
+
+        List<TestState> testStates = new ArrayList<>();
+
+        for (int i = 0; i < tests.length; i++) {
+            TestState state = launchTest(testClass, tests[i], beforeMethods, afterMethods);
+
+            testStates.add(state);
+        }
+
+        showStatistics(testClass, testStates);
+    }
+
+    private static void showStatistics(Class<?> testClass, List<TestState> testStates) {
+        System.out.println("Statistics for test launching for class " + testClass.getName() + ": ");
+
+        testStates.stream().forEach(System.out::println);
+    }
+
+    private static <T> TestState launchTest(Class<T> testClass, Method test, Method[] beforeMethods, Method[] afterMethods) {
+
+        try{
+            Constructor<T> cnstrtr = testClass.getDeclaredConstructor();
+            Object o = cnstrtr.newInstance();
+
+            for (int i = 0; i < beforeMethods.length; i++) {
+                beforeMethods[i].invoke(o);
+            }
+
+            test.invoke(o);
+
+            for (int i = 0; i < afterMethods.length; i++) {
+                afterMethods[i].invoke(o);
+            }
+
+            return new TestState(test.getName(), State.OK, null);
+        }catch(Exception exc){
+            return new TestState(test.getName(), State.FAILED, exc);
+        }
+    }
+
+    private enum State{
+        OK,
+        FAILED
+    }
+    private static class TestState{
+
+        private String name;
+        private State state;
+        private Exception exception;
+
+        public TestState(String name, State state, Exception exc){
+            this.name = name;
+            this.state = state;
+            this.exception = exc;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public State getState() {
+            return state;
+        }
+
+        public Exception getException() {
+            return exception;
+        }
+
+        @Override
+        public String toString(){
+            return this.name + "\t" + this.state + "\t" + this.exception.toString();
+        }
+    }
+}
