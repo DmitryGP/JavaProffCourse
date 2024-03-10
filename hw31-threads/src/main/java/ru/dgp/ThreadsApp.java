3fd65628b2645a14552fe68e96c710d82cf1802c
DmitryGP <dimgeo@yandex.ru>
2024-03-10 16:21:13 +0300

    Д.з. для лекции "Executors"

---
+++ hw31-threads/src/main/java/ru/dgp/ThreadsApp.java
@@ -0,0 +1,61 @@
+package ru.dgp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ThreadsApp {
+    private static final Logger logger = LoggerFactory.getLogger(ThreadsApp.class);
+
+    private int lastThreadNumber;
+
+    public static void main(String[] args) {
+        ThreadsApp threadsApp = new ThreadsApp();
+
+        new Thread(() -> threadsApp.action(1)).start();
+        new Thread(() -> threadsApp.action(2)).start();
+    }
+
+    private synchronized void action(int threadNum) {
+        try {
+
+            int count = 1;
+            short delta = 1;
+
+            while(true) {
+                while(lastThreadNumber == threadNum) {
+                    this.wait();
+                }
+
+                logger.info("Thread [%s]: %d".formatted(threadNum, count));
+
+                if(count == 10) {
+                    delta = -1;
+                }
+
+                if(count == 1) {
+                    delta = 1;
+                }
+
+                count = count + delta ;
+
+                lastThreadNumber = threadNum;
+
+                sleep();
+                notifyAll();
+            }
+
+
+        } catch(InterruptedException exc) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static void sleep() {
+        try {
+            Thread.sleep(1_000);
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage());
+            Thread.currentThread().interrupt();
+        }
+    }
+}
